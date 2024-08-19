4ded79e5e10181b74043782bedbd20aeb610d3ac
Sudhanshu Ranjan <127604679+sudh-12@users.noreply.github.com>
2024-08-20 03:31:12 +0530

    [650] [Time Beats: 76.72%] [Memory Beats: 56.27%] - LeetPush

--- 650-2-Keys-Keyboard.cpp
+++ 650-2-Keys-Keyboard.cpp
@@ -1,25 +1,43 @@
 class Solution {
     vector<vector<int>> dp;
-    int solve(int len , int num, int clipboard){
+    int solve(int len, int num, int clipboard) {
 
-    if(len==num)
-        return 0;
+        if (len == num)
+            return 0;
 
-        if(len>num)
-        return 1e9;
+        if (len > num)
+            return 1e9;
 
-        if(dp[len][clipboard]!=-1)
-        return dp[len][clipboard];
+        if (dp[len][clipboard] != -1)
+            return dp[len][clipboard];
 
-        int copy= 2+ solve(2*len, num, len);
-        int paste= 1 + solve(len+clipboard, num, clipboard); 
+        int copy = 2 + solve(2 * len, num, len);
+        int paste = 1 + solve(len + clipboard, num, clipboard);
 
-        return dp[len][clipboard]= min(copy, paste);
+        return dp[len][clipboard] = min(copy, paste);
     }
+
 public:
     int minSteps(int n) {
+
+        // TC : O(sqrt(n)) || o(n) in worst case(prime))
+        if (n == 1) return 0;
         
-        dp.resize(n,vector<int> (n,-1));
-        return n==1 ? 0 : 1 + solve(1, n, 1);
+        int steps = 0;
+        int factor = 2;
+        
+        while (n > 1) {
+            while (n % factor == 0) {
+                steps += factor;
+                n /= factor;
+            }
+            factor++;
+        }
+        
+        return steps;
+
+        // For DP Solution : TC O(n2)
+        // dp.resize(n, vector<int>(n, -1));
+        // return n == 1 ? 0 : 1 + solve(1, n, 1);
     }
 };
