0948123392af8af0d788fbb0aa360928f268fe36
Sudhanshu Ranjan <127604679+sudh-12@users.noreply.github.com>
2024-09-16 02:24:11 +0530

    [45] [Time Beats: 79.74%] [Memory Beats: 78.30%] - LeetPush

--- 45-Jump-Game-II.cpp
+++ 45-Jump-Game-II.cpp
@@ -5,25 +5,33 @@ public:
         int mx_jump = 0;
         int jump = 0;
 
+        //if already at last position return 0
         if (n == 1)
             return 0;
 
+    // the idea is to jump at that position from where you can jump farthest to next position
+    // because you have to greedly reach the end of array as soon as possible
+
         for (int i = 0; i < n; ++i) {
             mx_jump = i + nums[i];
+            // check if it touches the last position
             if (mx_jump >= n - 1)
                 return jump + 1;
             int pos = 0;
-            int mir = 0;
-            for (int j = i + 1; j <= i + nums[i]; ++j) {
-                if (j + nums[j] > mir) {
-                    mir = j + nums[j];
+            // check max we can go from a particular position in range of current steps
+            int max_in_range = 0;
+            for (int j = i + 1; j <= mx_jump; ++j) {
+                if (j + nums[j] > max_in_range) {
+                   max_in_range = j + nums[j];
                     pos = j;
                 }
             }
+            //i will increment 1 position as per for loop
             i = pos - 1;
             jump += 1;
         }
 
+        // in the worst case it will take n-1 jumps
         return n - 1;
     }
 };
